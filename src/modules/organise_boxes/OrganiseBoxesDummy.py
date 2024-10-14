044d201b7f3512a7c5a3e1e0c45772d9466f7a99
Mathias Vanhée <mathiasvanhee59@gmail.com>
2024-10-14 15:12:40 +0200

    [WIP] better fill of boxes : Now if can't fit a line in a box, it tries to fit the maximum products of that line.

--- src/modules/organise_boxes/OrganiseBoxesDummy.py
+++ src/modules/organise_boxes/OrganiseBoxesDummy.py
@@ -21,29 +21,32 @@ class OrganiseBoxesDummy(BaseOrganiseBoxesModule):
             for order_product_quantity_pair in order.products:
                 # left_qty : left quantity of the product to put in the boxes
                 left_qty = order_product_quantity_pair.quantity
+                product_weight = order_product_quantity_pair.product.weight
+                product_volume = order_product_quantity_pair.product.volume
 
-                line_weight = left_qty * order_product_quantity_pair.product.weight
-                line_volume = left_qty * order_product_quantity_pair.product.volume
+                line_weight = left_qty * product_weight
+                line_volume = left_qty * product_volume
 
-                while line_weight + order_boxes[i_box].used_weight > max_weight or line_volume + order_boxes[i_box].used_volume > max_volume:
-                    # first, we try to find how many individual products we can fit in the box
-                    n = 0
-                    while (n < left_qty
-                           and order_product_quantity_pair.product.weight * n + order_boxes[i_box].used_weight <= max_weight
-                           and order_product_quantity_pair.product.volume * n + order_boxes[i_box].used_volume <= max_volume):
-                        n += 1
-
+                while (line_weight + order_boxes[i_box].used_weight > max_weight
+                       or line_volume + order_boxes[i_box].used_volume > max_volume):
+                    # first, we try to find how many individual products we can fit in the box :
+                    # max_added_volume = max_volume - used_volume
+                    # n_volume = max_added_volume / product_volume (integer division)
+                    # max_added_weight = max_weight - used_weight
+                    # n_weight = max_added_weight / product_weight (integer division)
+                    # n = min(n_volume, n_weight) (number of products that can be added)
+                    n = min((max_volume - order_boxes[i_box].used_volume) // product_volume,
+                            (max_weight - order_boxes[i_box].used_weight) // product_weight)
                     # if we can fit at least one product, we add it to the box
                     if n > 0:
                         order_boxes[i_box].add_product_quantity_pair(ProductQuantityPair(order_product_quantity_pair.product, n))
                         left_qty -= n
-
                     # if we can't fit any product, we move to the next box
                     i_box += 1
+                    # if we are out of boxes, we stop
+                    if i_box >= len(order_boxes):
+                        raise NotImplementedError(f"The order {order.order_id} is too big to fit in the boxes")
 
-                # if we are out of boxes, we stop
-                if i_box >= len(order_boxes):
-                    raise NotImplementedError("The order is too big to fit in the boxes")
                 order_boxes[i_box].add_product_quantity_pair(ProductQuantityPair(order_product_quantity_pair.product, left_qty))
             boxes.extend(order_boxes)
         return boxes
